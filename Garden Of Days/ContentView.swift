a59bd00c0c502ec2f9dc77f5d18ba2bed38714fd
Mayank Sharma <sharmayank1608@gmail.com>
2025-12-15 11:54:29 +0530

    Initial Commit

---
+++ Garden Of Days/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Garden Of Days
+//
+//  Created by Mayank Sharma on 15/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Garden Of Days/Garden_Of_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Garden_Of_DaysApp.swift
+//  Garden Of Days
+//
+//  Created by Mayank Sharma on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Garden_Of_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
